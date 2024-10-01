d87b07b772ace43b3bc2e31de0e7e53a71cbb8ea
Murat Erkin Cicek <murerkinn@gmail.com>
2024-10-01 10:40:59 +0300

    Add sonner

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
 import '@/styles/globals.css'
 import '@xyflow/react/dist/style.css'
+
 import type { AppProps } from 'next/app'
 
 import { ThemeProvider } from '@/components/theme-provider'
+import { Toaster } from '@/components/ui/sonner'
 import { TooltipProvider } from '@/components/ui/tooltip'
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -15,6 +17,8 @@ export default function App({ Component, pageProps }: AppProps) {
       <TooltipProvider>
         <Component {...pageProps} />
       </TooltipProvider>
+
+      <Toaster />
     </ThemeProvider>
   )
 }
