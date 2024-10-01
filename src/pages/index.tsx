44f4f9d31534c5186d855df27dc295eebd83909a
Murat Erkin Cicek <murerkinn@gmail.com>
2024-10-01 10:41:46 +0300

    Update home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,36 @@
+import Layout from '@/components/layout'
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from '@/components/ui/resizable'
+import GeneticAlgorithmFlow from '@/modules/genetic-algorithms/components/genetic-algorithm-flow'
+import PopulationSettings from '@/modules/genetic-algorithms/components/population-settings'
+
 export default function HomePage() {
   return (
     <>
-      <div>
-        <h1>hello world</h1>
-      </div>
+      <Layout>
+        <ResizablePanelGroup direction="vertical">
+          <ResizablePanel defaultSize={80} minSize={40}>
+            <ResizablePanelGroup direction="horizontal">
+              <ResizablePanel defaultSize={30} minSize={20}>
+                <GeneticAlgorithmFlow />
+              </ResizablePanel>
+
+              <ResizableHandle withHandle />
+
+              <ResizablePanel defaultSize={70} className="p-4">
+                <PopulationSettings />
+              </ResizablePanel>
+            </ResizablePanelGroup>
+          </ResizablePanel>
+
+          <ResizableHandle withHandle />
+
+          <ResizablePanel defaultSize={20}>Three</ResizablePanel>
+        </ResizablePanelGroup>
+      </Layout>
     </>
   )
 }
